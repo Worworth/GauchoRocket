5916a54c85922811a15d36131f66688298f36719
Worworth <44878945+Worworth@users.noreply.github.com>
2021-12-06 20:14:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+include_once("config/Configuration.php");
+
+$module = isset($_GET["module"]) ? $_GET["module"] : "inicio" ;
+$action = isset($_GET["action"]) ? $_GET["action"] : "show" ;
+
+$configuration = new Configuration();
+$router = $configuration->createRouter( "createInicioController", "show");
+
+$router->executeActionFromModule($module,$action);
+
+?>
